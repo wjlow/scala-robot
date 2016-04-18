f6db84b225abffa0da17aa253361146b1cb9d9ab
Jack Low <wjlow@users.noreply.github.com>
2016-04-18 23:37:18 +1000

    Now able to read input from file and print result to StdOut.

--- src/main/scala/wjlow/Main.scala
+++ src/main/scala/wjlow/Main.scala
@@ -1,5 +1,36 @@
 package wjlow
 
+import wjlow.Commands.applyAll
+import wjlow.Parser._
+import wjlow.Robot.ToyRobot
+
 object Main {
-  def main(args: Array[String]) = println("Hello world!")
+
+  case class ProgramState(robot: Option[ToyRobot], commands: Seq[ToyRobot => ToyRobot], outputToPrint: Option[String])
+
+  def main(args: Array[String]): Unit = {
+    var currentRobot = None: Option[ToyRobot]
+    var currentCommands = Nil: Seq[ToyRobot => ToyRobot]
+
+    for (line <- io.Source.fromFile(args(0)).getLines()) {
+      val programState = robotProgram(line)(ProgramState(currentRobot, currentCommands, None))
+      currentRobot = programState.robot
+      currentCommands = programState.commands
+      programState.outputToPrint foreach println
+    }
+  }
+
+  def robotProgram(line: String)(programState: ProgramState): ProgramState = {
+    programState.robot match {
+      case Some(robot) =>
+        if (line == "REPORT") {
+          val newRobot = applyAll(robot, programState.commands)
+          ProgramState(Some(newRobot), Nil, Some(newRobot.toString))
+        } else {
+          ProgramState(programState.robot, programState.commands :+ parseCommand(line), None)
+        }
+      case None => ProgramState(parsePlace(line), Nil, None)
+    }
+  }
+
 }
---
+++ src/test/scala/wjlow/MainSpec.scala
@@ -0,0 +1,63 @@
+package wjlow
+
+import org.scalactic.TypeCheckedTripleEquals
+import org.scalatest.{FunSpec, Matchers}
+import wjlow.Main._
+
+class MainSpec extends FunSpec with TypeCheckedTripleEquals with Matchers {
+
+  describe("robotProgram") {
+
+    it("should Place, Move x2, Turn Left, Move and Report") {
+      val inputLines =
+        """
+          |PLACE 1,2,EAST
+          |MOVE
+          |MOVE
+          |LEFT
+          |MOVE
+          |REPORT
+          | """.stripMargin.split("\n").filter(s => !s.trim.isEmpty)
+
+      val states = getProgramStates(inputLines)
+      states(0).outputToPrint should ===(None)
+      states(1).outputToPrint should ===(None)
+      states(2).outputToPrint should ===(None)
+      states(3).outputToPrint should ===(None)
+      states(4).outputToPrint should ===(None)
+      states(5).outputToPrint should ===(None)
+      states(6).outputToPrint should ===(Some("3,3,NORTH"))
+    }
+
+    it("should ignore invalid commands before Place, Report, Move and Report") {
+      val inputLines =
+        """
+          |INVALID
+          |MOVE
+          |REPORT
+          |PLACE 1,2,EAST
+          |REPORT
+          |MOVE
+          |REPORT
+          | """.stripMargin.split("\n").filter(s => !s.isEmpty)
+
+      val states = getProgramStates(inputLines)
+      states(0).outputToPrint should ===(None)
+      states(1).outputToPrint should ===(None)
+      states(2).outputToPrint should ===(None)
+      states(3).outputToPrint should ===(None)
+      states(4).outputToPrint should ===(None)
+      states(5).outputToPrint should ===(Some("1,2,EAST"))
+      states(6).outputToPrint should ===(None)
+      states(7).outputToPrint should ===(Some("2,2,EAST"))
+    }
+
+  }
+
+  private def getProgramStates(inputLines: Array[String]): Array[ProgramState] = {
+    val robotPrograms = inputLines map (line => robotProgram(line)(_))
+    val initialState = ProgramState(None, Nil, None)
+    (robotPrograms scanLeft initialState)((state, f) => f(state))
+  }
+
+}
